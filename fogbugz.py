2232786ec6a4c02a03c3760cd7c778cfc96c2b55
Aske Merci <rainman1011@tuta.io>
2021-05-17 09:51:27 +0200

    Implemented setup function, thanks to @pernielsentikaer for making it

--- fogbugz.py
+++ fogbugz.py
@@ -7,15 +7,14 @@ if len(argv) == 1 or argv[1] == "t":
 if len(argv) != 1 and argv[1] == "w":
     print(getWeek())
 
-
 if len(argv) != 1 and argv[1] == "m":
     print(getMonth())
-
 
 if len(argv) != 1 and argv[1] == "a":
     print(getAll())
 
-
 if len(argv) != 1 and argv[1] == "missing":
     print(getMissing())
 
+if len(argv) == 1 or argv[1] == "setup":
+    print(setup())
--- lib/get/main.py
+++ lib/get/main.py
@@ -1,13 +1,15 @@
 import requests
 import math
 from ..tools.dateUtil import *
-import config
+import os
+import getpass
 
 now = datetime.now()
 
 
 def getToday():
-    start = now.strftime("%Y-%m-%d 06:00:00Z")
+    import config
+    start = now.strftime("%Y-%m-%d 00:00:00Z")
     end = now.strftime("%Y-%m-%d 23:59:59Z")
 
     payload = {
@@ -17,7 +19,8 @@ def getToday():
         "dtEnd": end
     }
 
-    response = requests.request("POST", config.api["endpoint"], json=payload, headers=config.api["headers"]).json()
+    response = requests.request("POST", "{}/api/listIntervals".format(config.api["endpoint"]), json=payload,
+                                headers=config.api["headers"]).json()
     arr = []
     for interval in response['data']['intervals']:
         dtStart = parser.parse(interval["dtStart"])
@@ -27,10 +30,11 @@ def getToday():
             dtEnd = parser.parse(interval["dtEnd"])
 
         arr.append(abs(dtEnd - dtStart).total_seconds())
-    return "Today: " + str(math.ceil(round(sum(arr) / 60 / 60, 2))) + " hours"
+    return "Today: " + str(round(sum(arr) / 60 / 60, 2)) + " hours"
 
 
 def getWeek():
+    import config
     dt = datetime.strptime(now.strftime("%Y-%m-%d"), "%Y-%m-%d")
     start = dt - timedelta(days=dt.weekday())
     end = start + timedelta(days=6)
@@ -42,7 +46,8 @@ def getWeek():
         "dtEnd": end.strftime(("%Y-%m-%d %H:%M:%S"))
     }
 
-    response = requests.request("POST", config.api["endpoint"], json=payload, headers=config.api["headers"]).json()
+    response = requests.request("POST", "{}/api/listIntervals".format(config.api["endpoint"]), json=payload,
+                                headers=config.api["headers"]).json()
     arr = []
     for interval in response['data']['intervals']:
         dtStart = parser.parse(interval["dtStart"])
@@ -55,6 +60,7 @@ def getWeek():
 
 
 def getMonth():
+    import config
     todayDate = datetime.today()
     start = todayDate.replace(month=todayDate.month, day=1)
     endOfMonth = todayDate.replace(day=28) + timedelta(days=4)
@@ -67,7 +73,8 @@ def getMonth():
         "dtEnd": end.strftime(("%Y-%m-%d 00:00:00Z"))
     }
 
-    response = requests.request("POST", config.api["endpoint"], json=payload, headers=config.api["headers"]).json()
+    response = requests.request("POST", "{}/api/listIntervals".format(config.api["endpoint"]), json=payload,
+                                headers=config.api["headers"]).json()
     arr = []
     for interval in response['data']['intervals']:
         dtStart = parser.parse(interval["dtStart"])
@@ -80,6 +87,7 @@ def getMonth():
 
 
 def getMissing():
+    import config
     dt = datetime.strptime(now.strftime("%Y-%m-%d"), "%Y-%m-%d")
     start = dt - timedelta(days=dt.weekday())
     end = start + timedelta(days=6)
@@ -91,7 +99,8 @@ def getMissing():
         "dtEnd": end.strftime(("%Y-%m-%d %H:%M:%S"))
     }
 
-    response = requests.request("POST", config.api["endpoint"], json=payload, headers=config.api["headers"]).json()
+    response = requests.request("POST", "{}/api/listIntervals".format(config.api["endpoint"]), json=payload,
+                                headers=config.api["headers"]).json()
     arr = []
     for interval in response['data']['intervals']:
         dtStart = parser.parse(interval["dtStart"])
@@ -110,12 +119,14 @@ def getMissing():
 
 
 def getAll():
+    import config
     payload = {
         "token": config.api["token"],
         "ixPerson": config.etc["ix"],
     }
 
-    response = requests.request("POST", config.api["endpoint"], json=payload, headers=config.api["headers"]).json()
+    response = requests.request("POST", "{}/api/listIntervals".format(config.api["endpoint"]), json=payload,
+                                headers=config.api["headers"]).json()
     arr = []
     for interval in response['data']['intervals']:
         dtStart = parser.parse(interval["dtStart"])
@@ -125,3 +136,55 @@ def getAll():
             dtEnd = parser.parse(interval["dtEnd"])
         arr.append(abs(dtEnd - dtStart).total_seconds())
     return "All time: " + str(round(sum(arr) / 60 / 60, 2)) + " hours"
+
+
+def setup():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../config.py')
+    if os.path.exists(path):
+        action = input("Config file already exists, do you want to override it? Y/n: ")
+        if action.lower() == "n":
+            return "10-4!"
+
+    endpoint = input("Insert your fogbugz url <https://your-subdomain.fogbugz.com>: ")
+    email = input("E-mail: ")
+    password = getpass.getpass('Password: ')
+    workHours = input("Insert your work hours: ")
+    if not "https://" in endpoint:
+        endpoint = "https://" + endpoint
+
+    payload = {
+        "email": email,
+        "password": password
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Origin": "",
+        "X-Requested-With": ""
+    }
+
+    response = requests.request("POST", "{}/api/logon".format(endpoint), json=payload, headers=headers).json()
+    if response['errors']:
+        return print('\n{}'.format(response['errors'][0]['message']))
+
+    token = response['data']['token']
+
+    payload = {"token": token}
+    response = requests.request("POST", "{}/api/viewPerson".format(endpoint), json=payload, headers=headers).json()
+    ix = response['data']['person']['ixPerson']
+    with open(path, 'w') as fp:
+        fp.write('api = dict(\n')
+        fp.write('	headers={\n')
+        fp.write('		"Content-Type": "application/json",\n')
+        fp.write('		"Origin": "",\n')
+        fp.write('		"X-Requested-With": ""\n')
+        fp.write('	},\n')
+        fp.write('	endpoint="{}",\n'.format(endpoint))
+        fp.write('	token="{}"\n'.format(token))
+        fp.write(')\n\n')
+        fp.write('etc = dict(\n')
+        fp.write('	workHours={},\n'.format(workHours))
+        fp.write('	ix={}\n'.format(ix))
+        fp.write(')\n')
+
+    return '\n{}'.format("Setup complete")
