43e799515cc5f0686a1b842fd00f5ba56862a3bf
Aske Merci <rainman1011@tuta.io>
2021-07-04 22:24:53 +0200

    Refac, should probably go with argparse in the future..

--- fogbugz.py
+++ fogbugz.py
@@ -1,20 +1,20 @@
 from sys import argv
 from lib.get.main import *
 
-if len(argv) == 1 or argv[1] == "t":
-    print(getToday())
+commands = {
+    "t": getToday(),
+    "w": getWeek(),
+    "m": getMonth(),
+    "a": getAll(),
+    "missing": getMissing(),
+}
 
-if len(argv) != 1 and argv[1] == "w":
-    print(getWeek())
-
-if len(argv) != 1 and argv[1] == "m":
-    print(getMonth())
-
-if len(argv) != 1 and argv[1] == "a":
-    print(getAll())
-
-if len(argv) != 1 and argv[1] == "missing":
-    print(getMissing())
-
-if len(argv) == 1 or argv[1] == "setup":
-    print(setup())
+if len(argv) == 2:
+    if argv[1] in commands:
+        print(commands[argv[1]])
+    elif argv[1] == "setup":
+        print(setup())
+    else:
+        print("Command not found!")
+else:
+    print("You need to specify a command.")
